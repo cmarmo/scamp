381318082c94939d5ffd4c37086a9159f559e3c2
Sébastien Serre <sebastien.serre@sysmo.io>
2017-10-25 15:08:52 +0200

    cleanup crossid.c and .h

--- src/crossid.h
+++ src/crossid.h
@@ -1,30 +1,30 @@
 /*
-*				crossid.h
-*
-* Include file for crossid.c
-*
-*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-*
-*	This file part of:	SCAMP
-*
-*	Copyright:		(C) 2002-2010 Emmanuel Bertin -- IAP/CNRS/UPMC
-*
-*	License:		GNU General Public License
-*
-*	SCAMP is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-* 	(at your option) any later version.
-*	SCAMP is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*	GNU General Public License for more details.
-*	You should have received a copy of the GNU General Public License
-*	along with SCAMP. If not, see <http://www.gnu.org/licenses/>.
-*
-*	Last modified:		10/10/2010
-*
-*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+ *    crossid.h
+ *
+ * Include file for crossid.c
+ *
+ *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ *
+ * This file part of: SCAMP
+ *
+ * Copyright:  (C) 2002-2010 Emmanuel Bertin -- IAP/CNRS/UPMC
+ *
+ * License:  GNU General Public License
+ *
+ * SCAMP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * SCAMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with SCAMP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modified:  10/10/2010
+ *
+ *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 #ifndef _CROSSID_H_
 #define _CROSSID_H_
@@ -36,10 +36,17 @@
 /*--------------------------------- typedefs --------------------------------*/
 /*------------------------------- functions ---------------------------------*/
 
-extern int	check_fieldoverlap(fieldstruct *field1, fieldstruct *field2),
-		check_fieldphotomoverlap(fieldstruct *field, int instru);
+extern int check_fieldoverlap(
+    fieldstruct *field1, 
+    fieldstruct *field2);
 
-extern void	crossid_fgroup(fgroupstruct *fgroup,
-				fieldstruct *reffield, double tolerance);
+extern int check_fieldphotomoverlap(
+    fieldstruct *field, 
+    int instru);
+
+extern void crossid_fgroup(
+    fgroupstruct *fgroup,
+    fieldstruct *reffield, 
+    double tolerance);
 
 #endif // _CROSSID_H_
